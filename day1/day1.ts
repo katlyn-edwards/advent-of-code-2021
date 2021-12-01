13fa022a6565f1d2744f69d9e4898ab439a8dbeb
Katlyn Edwards <kaitea.edwards@gmail.com>
2021-11-30 21:17:53 -0800

    Day 1
    I won!!!!!
    
    It probs won't happen again :P

---
+++ day1/day1.ts
@@ -0,0 +1,38 @@
+const input = `199
+200
+208
+210
+200
+207
+240
+269
+260
+263`;
+const inputArr = input.split(`\n`).map((el)=> Number(el));
+
+function partOne() {
+    let increased = 0;
+    for (let i = 1; i < input.length; i++) {
+        if (inputArr[i] > inputArr[i-1]) {
+            increased++;
+        }
+    }
+    console.log('Part One: ' + increased);
+}
+
+partOne();
+
+function partTwo() {
+    let currTotal;
+    let increased = 0;
+    for (let i = 2; i < input.length; i++) {
+        let total = inputArr[i] + inputArr[i - 1] + inputArr[i - 2];
+        if (!!currTotal && total > currTotal) {
+            increased++;
+        }
+        currTotal = total;
+    }
+    console.log('Part two: ' + increased);
+}
+
+partTwo();
