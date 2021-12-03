7bfbbe2af14a77e61e4d5dd574ec3da7d9035269
Katlyn Edwards <kaitea.edwards@gmail.com>
2021-12-02 21:35:08 -0800

    Day 3
    I got got by the question details. Gotta read the spec more closely ;__;

---
+++ day3/day3.ts
@@ -0,0 +1,74 @@
+import { EPIPE } from "constants";
+
+namespace day3 {
+    const input = `00100
+    11110
+    10110
+    10111
+    10101
+    01111
+    00111
+    11100
+    10000
+    11001
+    00010
+    01010`;
+    const inputArr = input.split(`\n`).map(line => line.trim());
+
+    function partOne() {
+        let gammaStr = '';
+        let epsilonStr = '';
+        for (let j = 0; j < 5; j++) {
+            const ret = getMaxMin(inputArr, j);
+            gammaStr += ret.common;
+            epsilonStr += ret.least;
+        }
+        const gamma = parseInt(gammaStr, 2);
+        const epsilon = parseInt(epsilonStr, 2);
+        console.log(`Part One: ${gamma * epsilon}`);
+    }
+    partOne();
+
+    function partTwo() {
+        let oxygen = inputArr.slice();
+        let c02 = inputArr.slice();
+        let index = 0;
+        while (oxygen.length > 1) {
+            const ret = getMaxMin(oxygen, index);
+            const compare = ret.equal ? '1' : ret.common;
+            oxygen = oxygen.filter(el => {
+                return el[index] == compare;
+            });
+            index++;
+        }
+        index = 0;
+        while (c02.length > 1) {
+            const ret = getMaxMin(c02, index);
+            const compare = ret.equal ? '0' : ret.least;
+            c02 = c02.filter(el => {
+                return el[index] == compare;
+            });
+            index++;
+        }
+        console.log(`Part two: ${parseInt(oxygen[0], 2) * parseInt(c02[0], 2)}`);
+    }
+    partTwo();
+
+    function getMaxMin(input: string[], index:  number): {common: string, least: string, equal: boolean} {
+        let ones = 0;
+            let zeros = 0;
+            for (let i = 0; i < input.length; i++) {
+                let line = input[i];
+                let char = line[index];
+                if (char == '0') {
+                    zeros++;
+                } else if (char == '1') {
+                    ones++;
+                }
+            }
+            const common = ones > zeros ? '1' : '0';
+            const least = ones < zeros ? '1' : '0';
+            const equal = common == least;
+            return {common, least, equal}
+    }
+}
---
+++ out/day3/day3.js
@@ -0,0 +1,69 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var day3;
+(function (day3) {
+    var input = "00100\n    11110\n    10110\n    10111\n    10101\n    01111\n    00111\n    11100\n    10000\n    11001\n    00010\n    01010";
+    var inputArr = input.split("\n").map(function (line) { return line.trim(); });
+    function partOne() {
+        var gammaStr = '';
+        var epsilonStr = '';
+        for (var j = 0; j < 5; j++) {
+            var ret = getMaxMin(inputArr, j);
+            gammaStr += ret.common;
+            epsilonStr += ret.least;
+        }
+        var gamma = parseInt(gammaStr, 2);
+        var epsilon = parseInt(epsilonStr, 2);
+        console.log("Part One: ".concat(gamma * epsilon));
+    }
+    partOne();
+    function partTwo() {
+        var oxygen = inputArr.slice();
+        var c02 = inputArr.slice();
+        var index = 0;
+        var _loop_1 = function () {
+            var ret = getMaxMin(oxygen, index);
+            var compare = ret.equal ? '1' : ret.common;
+            oxygen = oxygen.filter(function (el) {
+                return el[index] == compare;
+            });
+            index++;
+        };
+        while (oxygen.length > 1) {
+            _loop_1();
+        }
+        index = 0;
+        var _loop_2 = function () {
+            var ret = getMaxMin(c02, index);
+            var compare = ret.equal ? '0' : ret.least;
+            c02 = c02.filter(function (el) {
+                return el[index] == compare;
+            });
+            index++;
+        };
+        while (c02.length > 1) {
+            _loop_2();
+        }
+        console.log("Part two: ".concat(parseInt(oxygen[0], 2) * parseInt(c02[0], 2)));
+    }
+    partTwo();
+    function getMaxMin(input, index) {
+        var ones = 0;
+        var zeros = 0;
+        for (var i = 0; i < input.length; i++) {
+            var line = input[i];
+            var char = line[index];
+            if (char == '0') {
+                zeros++;
+            }
+            else if (char == '1') {
+                ones++;
+            }
+        }
+        var common = ones > zeros ? '1' : '0';
+        var least = ones < zeros ? '1' : '0';
+        var equal = common == least;
+        return { common: common, least: least, equal: equal };
+    }
+})(day3 || (day3 = {}));
+//# sourceMappingURL=day3.js.map
