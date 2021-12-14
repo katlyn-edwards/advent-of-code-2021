3bf4b744515e076fe4e3617b479c65d0f6bad775
Katlyn Edwards <kaitea.edwards@gmail.com>
2021-12-13 21:59:34 -0800

    @kwlabuda doing the heavy lifting

---
+++ day14/day14.ts
@@ -0,0 +1,142 @@
+namespace day13 {
+    const input = `NNCB
+
+    CH -> B
+    HH -> N
+    CB -> H
+    NH -> C
+    HB -> C
+    HC -> B
+    HN -> C
+    NN -> C
+    BH -> H
+    NC -> B
+    NB -> B
+    BN -> B
+    BB -> N
+    BC -> B
+    CC -> N
+    CN -> C`;
+
+    const clean = input.split(`\n`).map(el => el.trim());
+    const joined = clean.join('\n');
+    let parts = joined.split('\n\n');
+    let startCode = parts[0];
+    let instructions = parts[1];
+    let lines = instructions.split('\n');
+
+    let map = new Map<string, string>();
+    for (let line of lines) {
+        let p = line.split(' -> ');
+        map.set(p[0], p[1]);
+    }
+
+    function applyTransformations(map: Map<string, string>, start: string) {
+        let modifications = start.split('');
+        let inserted = 0;
+        for (let i = 1; i < start.length; i++) {
+            let pair = start[i - 1] + start[i];
+            if (map.has(pair)) {
+                let first = modifications.slice(0, i + inserted);
+                let second = modifications.slice(i + inserted);
+                modifications = [...first, map.get(pair), ...second];
+                inserted++;
+            }
+        }
+        return modifications;
+    }
+
+    function partOne() {
+        let code = startCode;
+        for (let i = 0; i < 10; i++) {
+            let result = applyTransformations(map, code);
+            code = result.join('');
+        }
+
+        let counts = new Map<string, number>();
+        let chars = code.split('');
+        chars.forEach(char => {
+            if (!counts.has(char)) {
+                counts.set(char, 0);
+            }
+            counts.set(char, counts.get(char) + 1);
+        });
+        let min = Number.MAX_SAFE_INTEGER; 
+        let max = 0;
+        counts.forEach((val, key) => {
+            if (val > max) {
+                max = val;
+            }
+            if (val < min) {
+                min = val;
+            }
+        });
+        console.log(`Part one: ${max - min} `);
+    }
+    partOne();
+
+    function applyTransformations2(map: Map<string, string>, start: Map<string, number>) {
+        let resultMap = new Map<string, number>();
+        start.forEach((counts, pair) => {
+            if (map.has(pair)) {
+                let transformChar = map.get(pair);
+                let totalCounts = counts;
+                let first = pair[0] + transformChar;
+                if (resultMap.has(first)) {
+                    totalCounts += resultMap.get(first);
+                }
+                resultMap.set(first, totalCounts);
+
+                let totalCounts2 = counts;
+                let second = transformChar + pair[1];
+                if (resultMap.has(second)) {
+                    totalCounts2 += resultMap.get(second);
+                }
+                resultMap.set(second, totalCounts2);
+            } else {
+                resultMap.set(pair, counts);
+            }
+        });
+        return resultMap;
+    }
+
+    function partTwo() {
+        let code = startCode;
+        let initialPairs = new Map<string, number>();
+        for (let i = 1; i < code.length; i++) {
+            let pair = code[i - 1] + code[i];
+            initialPairs.set(pair, 1);
+        };
+
+        for (let i = 0; i < 40; i++) {
+            let resultMap = applyTransformations2(map, initialPairs);
+            initialPairs = resultMap;
+        }
+
+        let counts = new Map<string, number>();
+        let lastLetter = startCode[startCode.length - 1];
+        initialPairs.forEach((count, pair) => {
+            pair.split('');
+            if (!counts.has(pair[0])) {
+                counts.set(pair[0], 0);
+            }
+            // Only count first item in pair, except for last one.
+            counts.set(pair[0], counts.get(pair[0]) + count);
+        });
+
+        counts.set(lastLetter, counts.get(lastLetter) + 1);
+
+        let min = Number.MAX_SAFE_INTEGER; 
+        let max = 0;
+        counts.forEach((val, key) => {
+            if (val > max) {
+                max = val;
+            }
+            if (val < min) {
+                min = val;
+            }
+        });
+        console.log(`Part two: ${max - min} `);
+    }
+    partTwo();
+}
---
+++ out/day14/day14.js
@@ -0,0 +1,127 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+var day13;
+(function (day13) {
+    var input = "NNCB\n\n    CH -> B\n    HH -> N\n    CB -> H\n    NH -> C\n    HB -> C\n    HC -> B\n    HN -> C\n    NN -> C\n    BH -> H\n    NC -> B\n    NB -> B\n    BN -> B\n    BB -> N\n    BC -> B\n    CC -> N\n    CN -> C";
+    var clean = input.split("\n").map(function (el) { return el.trim(); });
+    var joined = clean.join('\n');
+    var parts = joined.split('\n\n');
+    var startCode = parts[0];
+    var instructions = parts[1];
+    var lines = instructions.split('\n');
+    var map = new Map();
+    for (var _i = 0, lines_1 = lines; _i < lines_1.length; _i++) {
+        var line = lines_1[_i];
+        var p = line.split(' -> ');
+        map.set(p[0], p[1]);
+    }
+    function applyTransformations(map, start) {
+        var modifications = start.split('');
+        var inserted = 0;
+        for (var i = 1; i < start.length; i++) {
+            var pair = start[i - 1] + start[i];
+            if (map.has(pair)) {
+                var first = modifications.slice(0, i + inserted);
+                var second = modifications.slice(i + inserted);
+                modifications = __spreadArray(__spreadArray(__spreadArray([], first, true), [map.get(pair)], false), second, true);
+                inserted++;
+            }
+        }
+        return modifications;
+    }
+    function partOne() {
+        var code = startCode;
+        for (var i = 0; i < 10; i++) {
+            var result = applyTransformations(map, code);
+            code = result.join('');
+        }
+        var counts = new Map();
+        var chars = code.split('');
+        chars.forEach(function (char) {
+            if (!counts.has(char)) {
+                counts.set(char, 0);
+            }
+            counts.set(char, counts.get(char) + 1);
+        });
+        var min = Number.MAX_SAFE_INTEGER;
+        var max = 0;
+        counts.forEach(function (val, key) {
+            if (val > max) {
+                max = val;
+            }
+            if (val < min) {
+                min = val;
+            }
+        });
+        console.log("Part one: ".concat(max - min, " "));
+    }
+    partOne();
+    function applyTransformations2(map, start) {
+        var resultMap = new Map();
+        start.forEach(function (counts, pair) {
+            if (map.has(pair)) {
+                var transformChar = map.get(pair);
+                var totalCounts = counts;
+                var first = pair[0] + transformChar;
+                if (resultMap.has(first)) {
+                    totalCounts += resultMap.get(first);
+                }
+                resultMap.set(first, totalCounts);
+                var totalCounts2 = counts;
+                var second = transformChar + pair[1];
+                if (resultMap.has(second)) {
+                    totalCounts2 += resultMap.get(second);
+                }
+                resultMap.set(second, totalCounts2);
+            }
+            else {
+                resultMap.set(pair, counts);
+            }
+        });
+        return resultMap;
+    }
+    function partTwo() {
+        var code = startCode;
+        var initialPairs = new Map();
+        for (var i = 1; i < code.length; i++) {
+            var pair = code[i - 1] + code[i];
+            initialPairs.set(pair, 1);
+        }
+        ;
+        for (var i = 0; i < 40; i++) {
+            var resultMap = applyTransformations2(map, initialPairs);
+            initialPairs = resultMap;
+        }
+        var counts = new Map();
+        var lastLetter = startCode[startCode.length - 1];
+        initialPairs.forEach(function (count, pair) {
+            pair.split('');
+            if (!counts.has(pair[0])) {
+                counts.set(pair[0], 0);
+            }
+            // Only count first item in pair, except for last one.
+            counts.set(pair[0], counts.get(pair[0]) + count);
+        });
+        counts.set(lastLetter, counts.get(lastLetter) + 1);
+        var min = Number.MAX_SAFE_INTEGER;
+        var max = 0;
+        counts.forEach(function (val, key) {
+            if (val > max) {
+                max = val;
+            }
+            if (val < min) {
+                min = val;
+            }
+        });
+        console.log("Part two: ".concat(max - min, " "));
+    }
+    partTwo();
+})(day13 || (day13 = {}));
+//# sourceMappingURL=day14.js.map
